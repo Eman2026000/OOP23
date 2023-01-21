77add37cb616755f9125b23cb52d5c2ae5d9f544
Aeman <91747112+Eman2026000@users.noreply.github.com>
2023-01-20 21:05:02 -0500

    i

--- lsp-spring2023/src/org/howard/edu/hw1/helloworld.java
+++ lsp-spring2023/src/org/howard/edu/hw1/helloworld.java
@@ -3,8 +3,8 @@ package org.howard.edu.hw1;
 public class helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+     System.out.println("Hello World");
+     
 	}
 
 }
