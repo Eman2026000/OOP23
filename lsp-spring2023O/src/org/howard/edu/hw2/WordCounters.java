b4b6e87d41768ee513334e1bcd55af377a34fb51
Aeman <91747112+Eman2026000@users.noreply.github.com>
2023-02-03 00:55:06 -0500

    the secound commit

---
+++ lsp-spring2023O/src/org/howard/edu/hw2/WordCounters.java
@@ -0,0 +1,62 @@
+package org.howard.edu.hw2;
+
+import java.io.*;
+import org.howard.lsp.utils.FileReader;
+import java.util.HashMap;
+
+
+//Author: Aeman Almotairi (@02959924)
+
+public class WordCounters {
+	
+	/**
+	 * this method reads a text file and counts the number of times each word that it contains appears
+	 * @param args
+	 * @throws FileNotFoundException
+	 */
+
+	
+	public static void main(String[] args) throws FileNotFoundException {
+	    // create a new  FileReader
+		FileReader fr = new FileReader();
+		
+		// Create a HashMap to store the word count
+		HashMap<String, Integer> Counter = new HashMap<>();
+		
+		// read text from file 'fr' and store it into the variable 'inputs'
+		String  inputs= fr.readToString("main/resources/words.txt");
+		
+		// Split the text into words
+		String[] words =inputs.split(" ") ;
+		
+		
+		 for (String word: words) {
+         	// Check the length of the word and if it contains only alphabets
+         	if(word.length() > 3 && word.matches(".*[a-zA-Z]+.*")){
+         		
+         		// Convert to Lower Case
+         		word = word.toLowerCase();
+         		
+         		//increment the count for each word
+         		if (Counter.containsKey(word)) {
+       
+         			Counter.put(word, Counter.get(word) + 1);
+         			
+         		} else {
+         			
+         			Counter.put(word, 1);
+         		}
+         		
+         	}
+         }
+		
+		 
+		 //// Print the result
+		 for (String word: Counter.keySet()) {
+             System.out.println(word + "\t" + Counter.get(word));
+         }
+		
+		
+            
+}
+}
