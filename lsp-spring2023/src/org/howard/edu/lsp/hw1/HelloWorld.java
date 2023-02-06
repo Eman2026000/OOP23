a0f24d748ccc284c9e8d283b110f53998dfc3f5b
Aeman <91747112+Eman2026000@users.noreply.github.com>
2023-02-05 23:23:55 -0500

    Change project structure

---
+++ lsp-spring2023/src/org/howard/edu/lsp/hw1/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.hw1;
+
+//Author: Aeman Almotairi (@02959924)
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+     System.out.println("Hello World");
+     
+	}
+
+}
